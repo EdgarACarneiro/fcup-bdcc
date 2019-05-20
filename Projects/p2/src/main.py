f6ffda7b53cd37c1bc632c5840c029d327b35daf
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-05-20 12:36:08 +0100

    Minor progress

--- Projects/p2/src/main.py
+++ Projects/p2/src/main.py
@@ -1,5 +1,37 @@
 import apache_beam as beam
-from beam.io import TextIO
+import argparse
 
 #beam.io.Read(beam.io.TextFileSource('gs://big_events/EVENTS.csv.gz'), compression_type='GZIP')
-#TextIO.Read.from('gs://big_events/EVENTS.csv.gz').withCompressionType(TextIO.CompressionType.GZIP)
+# TextIO.Read.from('gs://big_events/EVENTS.csv.gz').withCompressionType(TextIO.CompressionType.GZIP)
+
+
+class Split(beam.DoFn):
+
+    # TODO: Change this to automatically read the schema and process it
+    def process(self, element):
+        country, duration, user = element.split(",")
+
+        return [{
+            'country': country,
+            'duration': float(duration),
+            'user': user
+        }]
+
+def process(args):
+    with beam.Pipeline() as p:
+
+        rows = (
+            p |
+            beam.io.ReadFromText(args.input_file) |
+            beam.ParDo(Split())
+        )
+
+if __name__ == '__main__':
+    """Main function"""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-i', '--input_file', required=True,
+                        help='Input csv file containing the data')
+
+    process(parser.parse_args)
