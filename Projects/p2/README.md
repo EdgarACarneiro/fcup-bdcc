aeb3449e1eaa9e4d06d35c3f84c0facc54bea557
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-05-27 19:47:41 +0100

    Filtering given patient data

--- Projects/p2/README.md
+++ Projects/p2/README.md
@@ -8,3 +8,7 @@ mkvirtualenv test
 pip install -r -requirements.txt
 ```
 See this [tutorial](https://www.andreagrandi.it/2018/12/19/installing-python-and-virtualenv-on-osx/) for more information.
+
+# Helpers:
+* [Apache Beam Programming Guide](https://beam.apache.org/documentation/programming-guide/)
+* [Apache Beam: a python example](https://medium.com/@brunoripa/apache-beam-a-python-example-5644ca4ed581)
--- Projects/p2/src/main.py
+++ Projects/p2/src/main.py
@@ -5,19 +5,21 @@ import argparse
 """Your task is to perform a statistical analysis on this data
 and produce timeline graphs for each patient (SUBJECT_ID)"""
 
-# https://medium.com/@brunoripa/apache-beam-a-python-example-5644ca4ed581
-class Filter(beam.DoFn):
-    """DoFn to filter the columns of interest to us"""
 
-    def process(self, element):
-        _, subject_id, hadm_id = element.split(",")
+class FilterPatient(beam.DoFn):
+    """DoFn to filter patient's data"""
 
+    def __init__(self, patient_id):
+        self.patient_id = patient_id
 
-        return [{
-            'patient': subject_id,
-            'hospital_admission_id': hadm_id,
-            'user': user
-        }]
+    def process(self, elem):
+        # Since python2 hasn't star operator
+        el_data = elem.split(",")
+
+        # Only return if it's this patient data
+        if el_data[1] == self.patient_id:
+            return el_data[2:]
+
 
 class CollectionPrinter(beam.DoFn):
     """Helper DoFn able to print a PCollection contents"""
@@ -35,19 +37,17 @@ def run(args):
     options = PipelineOptions()
     with beam.Pipeline(options=options) as p:
 
-        csv_data = (
+        patient_data = (
             p |
-            'Reading Events' >> beam.io.ReadFromText(args.input_file, skip_header_lines=1)
+            'Reading Events' >> beam.io.ReadFromText(
+                args.input_file, skip_header_lines=1) |
+            'Get client data' >> beam.ParDo(
+                FilterPatient(args.patient))
         )
 
-        # TODO: Hardcoded columns for now, later read Schema dinamically
-        # -> Apache cant read csv's in fashion,  do it manually
-        filtered_data = (
-            csv_data | 
-            'Get columns of interest' >> beam.ParDo(Filter())
-        )
+        print_collection(patient_data)
 
-        #print_collection(csv_data)
+        # Call different implemented extractors here
 
 
 if __name__ == '__main__':
@@ -58,8 +58,8 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input_file', required=True,
                         help='Input csv file containing the data')
     parser.add_argument('-o', '--output_folder', default='',
-                    help='Output folder for the generated plots')
+                        help='Output folder for the generated plots')
     parser.add_argument('-p', '--patient', required=True,
-                    help='Patient that will analysed')
+                        help='Patient that will analysed')
 
     run(parser.parse_args())
