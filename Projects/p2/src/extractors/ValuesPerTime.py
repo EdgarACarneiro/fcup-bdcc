fce0aafa6d6ef94f7f1358bf76c27faf381721e4
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-06-05 11:55:53 +0100

    Refactored ValuesPerTime Extractor

--- Projects/p2/src/extractors/ValuesPerTime.py
+++ Projects/p2/src/extractors/ValuesPerTime.py
@@ -8,20 +8,35 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 
-class Process(beam.DoFn):
-    """DoFn to filter patient's data"""
-
-    def process(self, elem):
-        return [[time.mktime(parse(elem[3]).timetuple()),
-                 float(elem[7])]]
-
-
 class ValuesPerTime(AbstractExtractor):
 
     def __init__(self, name):
         super(ValuesPerTime, self).__init__(name)
 
-        self.process = Process()
+        self.process = \
+            lambda elem: [[time.mktime(parse(elem[3]).timetuple()),
+                           float(elem[7])]]
+
+    def output_data(self, data_list, dataframe, output_folder):
+        for entry in data_list:
+            dataframe = dataframe.append(
+                {
+                    'datetime': entry[0],
+                    'value': entry[1]
+                },
+                ignore_index=True
+            )
+
+        plt.style.use('seaborn')
+        plt.plot('datetime', 'value', data=dataframe, marker='o',
+                 markersize=4, color='mediumvioletred', linestyle='none')
+
+        plt.title('Values per Date', loc='left',
+                  fontsize=12, fontweight=0, color='black')
+        plt.xlabel('Datetime (in ms since 01/01/1970)')
+        plt.ylabel('Values')
+
+        plt.savefig('%s/%s.png' % (output_folder, self.name))
 
     def plot(self, p_collection, output_folder):
         # Creating Pandas Dataframe
@@ -29,26 +44,6 @@ class ValuesPerTime(AbstractExtractor):
 
         p_collection | \
             'Gathering Data on List' >> beam.combiners.ToList() | \
-            'Output data as plot' >> beam.ParDo(
-                lambda data: output_data(data, df, output_folder, self.name)
+            'Output data as a plot' >> beam.ParDo(
+                lambda data: self.output_data(data, df, output_folder)
             )
-
-
-def output_data(data_list, dataframe, output_folder, name):
-    for entry in data_list:
-        dataframe = dataframe.append(
-            {'datetime': entry[0],
-             'value': entry[1]},
-            ignore_index=True
-        )
-
-    plt.style.use('seaborn')
-    plt.plot('datetime', 'value', data=dataframe, marker='o',
-             markersize=4, color='mediumvioletred', linestyle='none')
-
-    plt.title('Values per Date', loc='left',
-              fontsize=12, fontweight=0, color='black')
-    plt.xlabel('Datetime (in ms since 01/01/1970)')
-    plt.ylabel('Values')
-
-    plt.savefig('%s/%s.png' % (output_folder, name))
