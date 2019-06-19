5897d6d5901a22a43b5bab1964f45e0b6407751b
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-06-19 23:12:56 +0100

    Updated last missing plot

--- Projects/p2/src/extractors/CGItemQuantity.py
+++ Projects/p2/src/extractors/CGItemQuantity.py
@@ -17,24 +17,42 @@ class CGItemQuantity(AbstractExtractor):
         self.process = \
             lambda el: [(
                 int(el[5]),
-                int(el[2]),
+                el[2],
                 float(el[7])
             )]
+
+    @staticmethod
+    def __round_to_base(value, base=5):
+        return int(base * round(float(value)/base))
 
     def output_data(self, data, output_folder):
         self.resetPlotting()
 
+        # Getting max and min to find round base
+        max_v = max(data, key=lambda el: el[2])[2]
+        min_v = min(data, key=lambda el: el[2])[2]
+        round_base = round((max(data, key=lambda el: el[2])[2] -
+                            min(data, key=lambda el: el[2])[2]) / 6)
+
         df = pd.DataFrame(
             columns=["Care Giver", "Item", "Quantity"],
-            data=data
+            data=map(lambda el:
+                     (el[0],
+                      el[1],
+                      self.__round_to_base(el[2], round_base)),
+                     data)
         )
 
-        parallel_coordinates(df, "Item", colormap=ListedColormap(
-            sns.color_palette("GnBu", 10)))
+        sns.cubehelix_palette(dark=.3, light=.8, as_cmap=True)
+        sns.scatterplot(x="Item", y="Care Giver",
+                        hue="Quantity", size="Quantity",
+                        sizes=(50, 300), hue_norm=(min_v, max_v),
+                        legend="full", data=df)
 
         plt.legend(loc='upper right')
         plt.title('CGI - Item - Quantity Relation', loc='left',
                   fontsize=12, fontweight=0, color='black')
+        plt.xticks(rotation='vertical')
 
         plt.savefig('%s/%s.png' % (output_folder, self.name))
 
